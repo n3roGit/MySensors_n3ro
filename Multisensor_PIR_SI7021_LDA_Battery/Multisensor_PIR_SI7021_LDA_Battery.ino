a5dc39f8e81a803b13374da0cb86ad1369ba8443
n3ro <christoph.bubeck@chbu.org>
2015-11-22 15:47:17 +0100

    added some extra output and force sensing counter

--- Multisensor_PIR_SI7021_LDA_Battery/Multisensor_PIR_SI7021_LDA_Battery.ino
+++ Multisensor_PIR_SI7021_LDA_Battery/Multisensor_PIR_SI7021_LDA_Battery.ino
@@ -4,14 +4,29 @@
 #include <Wire.h>
 #include <SI7021.h>
 
+//#define DEBUG
+#ifdef DEBUG
+#define DEBUG_SERIAL(x) Serial.begin(x)
+#define DEBUG_PRINT(x) Serial.print(x)
+#define DEBUG_PRINTLN(x) Serial.println(x)
+#else
+#define DEBUG_SERIAL(x)
+#define DEBUG_PRINT(x) 
+#define DEBUG_PRINTLN(x) 
+#endif
+
 #define NODE_ID 25                      // ID of node
 #define SLEEP_TIME 1800000                 // Sleep time between reports (in milliseconds)
 // 1h = 3600000
 // 30m = 1800000
 // 15m = 900000
 
+#define FORCE_TRANSMIT_CYCLE 36  // 5min*12=1/hour, 5min*36=1/3hour 
+#define BATTERY_REPORT_CYCLE 2880   // Once per 5min   =>   12*24*7 = 2016 (one report/week)
+
+
 //Node interrupt wakeup modes
-#define wakeUpMode RISING //RISING, CHANGE or FALLING
+#define wakeUpMode CHANGE //RISING, CHANGE or FALLING
 
 #define CHILD_ID_PIR 1                   // ID of the sensor PIR
 #define CHILD_ID_HUM 2                   // ID of the sensor HUM
@@ -19,14 +34,18 @@
 #define CHILD_ID_LIGHT 4                 // ID of the sensor LIGHT
 
 #define PIR_SENSOR_DIGITAL 3             // PIR pin
-#define HUMIDITY_SENSOR_DIGITAL_PIN 4    // DHT pin
+//#define HUMIDITY_SENSOR_DIGITAL_PIN 4    // DHT pin
 #define LIGHT_SENSOR_ANALOG_PIN 0        // LDR pin
 #define LED_PIN A1                       // LED connected PIN 
 //#define STEPUP_PIN 6                     // StepUp Transistor 
 //#define STEPUP_PIN2 7                    // StepUp Transistor
 
-// testing
+
+// Temp and Hum
 SI7021 si7021;
+#define HUMI_TRANSMIT_THRESHOLD 3.0  // THRESHOLD tells how much the value should have changed since last time it was transmitted.
+#define TEMP_TRANSMIT_THRESHOLD 0.5
+#define AVERAGES 2
 
 #define MIN_V 1900 // empty voltage (0%)
 #define MAX_V 3200 // full voltage (100%)
@@ -43,17 +62,29 @@ MyMessage msgLight(CHILD_ID_LIGHT, V_LIGHT_LEVEL);
 
 
 
-float lastTemp;
-float lastHum;
+float lastTemp = -100;
+float lastHum = -100;
+int batteryReportCounter = BATTERY_REPORT_CYCLE - 1;  // to make it report the first time.
+int measureCount = 0;
 //boolean metric = true;
 int oldBatteryPcnt;
-int lastLightLevel;
-int lastPIR;
+int lastLightLevel = -100;
+int lastPIR = -1;
 int repeatSending = 10;
 
 
 void setup()
 {
+  DEBUG_SERIAL(115200);    
+  DEBUG_PRINTLN("Serial started");
+  
+  DEBUG_PRINT("Voltage: ");
+  DEBUG_PRINT(readVcc()); 
+  DEBUG_PRINTLN(" mV");
+
+
+
+
   gw.begin(NULL, NODE_ID, false);
 
   gw.sendSketchInfo("PIR,si7021,Light", "1.0");
@@ -67,7 +98,7 @@ void setup()
   pinMode(PIR_SENSOR_DIGITAL, INPUT);
   //digitalWrite(PIR_SENSOR_DIGITAL, HIGH); //Use this if no external pullup is used for PIR
 
-  //si7021 PINS
+  //si7021 initial
   si7021.begin();
   
   //STEPUP PINS
@@ -81,12 +112,24 @@ void setup()
 void loop()
 {
   Serial.println("Waking up...");
-  sendPir();
-  sendLight();
-  sendTemp();
-  sendHum();
+  measureCount ++;
+  batteryReportCounter ++;
+  bool forceTransmit = false;
+  
+  if (measureCount > FORCE_TRANSMIT_CYCLE) {
+    forceTransmit = true; 
+  }
+  
+  
+  sendPir(forceTransmit);
+  sendLight(forceTransmit);
+  sendTemp(forceTransmit);
+  sendHum(forceTransmit);
+  
   sendBattery();
-
+  Serial.println(measureCount);
+  Serial.println(batteryReportCounter);
+  
   Serial.println("Going to sleep...");
   Serial.println("");
   gw.sleep(PIR_SENSOR_DIGITAL - 2, wakeUpMode, SLEEP_TIME);
@@ -95,8 +138,14 @@ void loop()
 
 void sendBattery() // Measure battery
 {
+  bool force = false;
   int batteryPcnt = min(map(readVcc(), MIN_V, MAX_V, 0, 100), 100);
-  if (batteryPcnt != oldBatteryPcnt) {
+  if (batteryReportCounter >= BATTERY_REPORT_CYCLE) {
+    force = true;
+    batteryReportCounter = 0;
+    }
+  if (batteryPcnt != oldBatteryPcnt or force == true) {
+    Serial.print(force ? "forced " : "normal ");
     gw.sendBatteryLevel(batteryPcnt); // Send battery percentage
     oldBatteryPcnt = batteryPcnt;
   }
@@ -106,11 +155,12 @@ void sendBattery() // Measure battery
 
 
 
-void sendPir() // Get value of PIR
+void sendPir(bool force) // Get value of PIR
 {
   int pir = digitalRead(PIR_SENSOR_DIGITAL); // Get value of PIR
-  if (pir != lastPIR) { // If status of PIR has changed
+  if (pir != lastPIR or force == true) { // If status of PIR has changed
     //gw.send(msgPir.set(pir == HIGH ? 1 : 0)); // Send PIR status to gateway
+    Serial.print(force ? "forced " : "normal ");
     resend((msgPir.set(pir == HIGH ? 1 : 0)),repeatSending);
     lastPIR = pir;
   }
@@ -120,15 +170,17 @@ void sendPir() // Get value of PIR
 
 
 
-void sendTemp() // Get temperature
+void sendTemp(bool force) // Get temperature
 {
   float temperature = si7021.getCelsiusHundredths();
   temperature = temperature / 100;
+  float diffTemp = abs(lastTemp - temperature);
   if (isnan(temperature)) {
     Serial.println("Failed reading temperature from si7021");
   } else {
-    if (temperature != lastTemp) {
+    if (diffTemp > TEMP_TRANSMIT_THRESHOLD or force == true) {
       //gw.send(msgTemp.set(temperature, 1));
+      Serial.print(force ? "forced " : "normal ");
       resend((msgTemp.set(temperature, 1)),repeatSending);
       lastTemp = temperature;
     }
@@ -138,14 +190,16 @@ void sendTemp() // Get temperature
 }
 
 
-void sendHum() // Get humidity
+void sendHum(bool force) // Get humidity
 {
   float humidity = si7021.getHumidityPercent();
+  float diffHum = abs(lastHum - humidity);
   if (isnan(humidity)) {
     Serial.println("Failed reading humidity from si7021");
   } else {
-    if (humidity != lastHum) {
+    if (diffHum > HUMI_TRANSMIT_THRESHOLD or force == true) {
       //gw.send(msgHum.set(humidity, 1));
+      Serial.print(force ? "forced " : "normal ");
       resend((msgHum.set(humidity, 1)),repeatSending);
       lastHum = humidity;
     }
@@ -155,11 +209,12 @@ void sendHum() // Get humidity
 }
 
 
-void sendLight() // Get light level
+void sendLight(bool force) // Get light level
 {
   int lightLevel = (1023 - analogRead(LIGHT_SENSOR_ANALOG_PIN)) / 10.23;
-  if (lightLevel != lastLightLevel) {
+  if (lightLevel != lastLightLevel or force == true) {
     //gw.send(msgLight.set(lightLevel));
+    Serial.print(force ? "forced " : "normal ");
     resend((msgLight.set(lightLevel)),repeatSending);
     lastLightLevel = lightLevel;
   }
